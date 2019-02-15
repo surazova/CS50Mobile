a81eb3a38f353e799eb626c606b9b229df3deb59
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-15 00:46:39 +0000

    Added imperative slide

---
+++ reactPropsState/imperativeSlide.js
@@ -0,0 +1,15 @@
+const SLIDE = {
+  title: 'React is declarative',
+  bullets: [
+    'Imperative vs Declarative',
+    "The browser APIs aren't fun to work with ",
+    'React allows us to write what we want, and the library will take care of the DOM manipulation',
+  ],
+}
+
+function createSlide(slide) {
+  const slideElement = document.createElement('div')
+
+  // TODO: Add a slide
+  const title = document.createElement('')
+}
